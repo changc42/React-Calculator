f3c1907f3e2ee08e3345f7b771a5bf5bf0b4a119
Rocky Singh <rockygsingh@icloud.com>
2020-01-10 19:32:59 -0500

    created calculator and screen component

---
+++ src/component/calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class calculator extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default calculator;
---
+++ src/component/screen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class screen extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default screen;
