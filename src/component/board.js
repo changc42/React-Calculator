e92fe28551909e316ab792424691f16684ee2327
Caleb Chang <changc42@gmail.com>
2020-01-10 21:18:32 -0500

    added buttons for board component. styled the buttons so every button except the numerical buttons have a gray background.

--- src/component/board.js
+++ src/component/board.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import '../CSS/board.css';
 
 export default class componentName extends Component {
   render() {
     return (
       <div> 
-        <h1>This will be the board</h1>
+        <table className="board">
+          <tr>
+            <td>(</td>
+            <td>)</td>
+            <td>n/a</td>
+            <td>AC</td>
+          </tr>
+          <tr>
+            <td className="number">7</td>
+            <td className="number">8</td>
+            <td className="number">9</td>
+            <td>/</td>
+          </tr>
+          <tr>
+            <td className="number">4</td>
+            <td className="number">5</td>
+            <td className="number">6</td>
+            <td>x</td>
+          </tr>
+          <tr>
+            <td className="number">1</td>
+            <td className="number">2</td>
+            <td className="number">3</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td className="number">0</td>
+            <td>.</td>
+            <td>=</td>
+            <td>+</td>
+          </tr>
+        </table>
       </div>
     );
   }
--- src/component/screen.js
+++ src/component/screen.js
@@ -10,7 +10,6 @@ class screen extends Component {
                 <div className="screen">
                 <span className="value">0</span>
                 </div>
-                <h1>This is the screen</h1>
             </div>
         );
     }
