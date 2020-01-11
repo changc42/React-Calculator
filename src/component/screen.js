a964e09ea1c0b0f7b2f23b22781501b4922f6e46
Rocky Singh <rockygsingh@icloud.com>
2020-01-10 20:13:14 -0500

    add screen css and rendered the screen

--- src/component/screen.js
+++ src/component/screen.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import '../CSS/screen.css'
 
 class screen extends Component {
     render() {
         return (
             <div>
+                
+
+                <div className="screen">
+                <span className="value">0</span>
+                </div>
                 <h1>This is the screen</h1>
             </div>
         );
