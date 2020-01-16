9a2840f2a519e06c0d2ec226dd2fbefbde684125
Rocky Singh <rockygsingh@icloud.com>
2020-01-16 16:14:34 -0500

    updated updateScreenDisplay function && imported evaluate

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -12,14 +12,15 @@ class calculator extends Component {
             mainD: "0", //mainD = main display
             subD: "", //subDisplay
             prevClick: "n",//prevClick can have one of three values: n(number), o(operation), or e(equals)
-            prent: ""
+            prent: "",
+            gray: false
          }
         this.updateScreenDisplay = this.updateScreenDisplay.bind(this)
     }
 
     // updates the state on the screen
     updateScreenDisplay(s) {
-        let {mainD, subD, prevClick} = this.state;
+        let {mainD, subD, prevClick, gray} = this.state;
         if(s === 'AC') {
             this.setState({
                 mainD: "0",
@@ -27,12 +28,12 @@ class calculator extends Component {
                 prevClick: "n",
             });
         }
-        else if(prevClick=="n"){
+        else if(prevClick === "n" && !gray){
             if(!isNaN(s)){
                 if(mainD=="0") this.setState({ mainD: s, prevClick: "n" });
                 else this.setState({ mainD: mainD+s, prevClick: "n" });
             }
-            else if(s === '/' || s === '+' || s === '-' || s === 'x'){
+            else if(s === '/' || s === '+' || s === '-' || s === 'x' && !gray){
                 this.setState({
                     prevClick: "o",
                     mainD: mainD+s,
@@ -119,22 +120,52 @@ class calculator extends Component {
                 console.log("equals sign pressed repetitively");
             }
         } // end of if else for "e"
-        else if(prevClick === "open") {
+        else if(prevClick === "open" || gray) {
+            let currVal = mainD.slice(0, mainD.length-1);
             if(!isNaN(s)) {
-                let currVal = mainD.slice(0, mainD.length-1);
                 this.setState({
                     mainD: currVal + s + ")",
-                    prevClick: "n"
+                    prevClick: "n",
+                    gray: true
+                })
+            } else if(s === '/' || s === '+' || s === '-' || s === 'x') {
+                this.setState({
+                    mainD: currVal +  s + ")",
+                    gray: true,
+                    prevClick: "o"
+                })
+            }  else if(s === "e") {
+                let result = evaluate(s);
+                this.setState({
+                    mainD: result
+                })
+            } else if(s === "(") {
+                this.setState({
+                    mainD: mainD
+                })
+            }else if(s === ")"){
+                this.setState({
+                    mainD: currVal + s,
+                    gray: false,
+                    prevClick: "close"
                 })
             }
-        }
+        } // if of open
+        else if(prevClick === 'close') {
+            this.setState({
+                mainD: mainD + s,
+                prevClick: "close"
+
+            })
+
+        } // end of close
         
     }
 
     render() {
         return (
             <div className="calculator">
-                <Screen prevClick={this.state.prevClick} mainD={this.state.mainD} subD={this.state.subD} />
+                <Screen gray={this.state.gray} prevClick={this.state.prevClick} mainD={this.state.mainD} subD={this.state.subD} />
                 <Board updateScreenDisplay={this.updateScreenDisplay}/>
             </div>
         );
